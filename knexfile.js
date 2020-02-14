b469f435156f3a58873a319bcf58ab801d0b74b3
vianarafael <vianarafaelds@gmail.com>
2020-02-14 17:15:43 +0900

    fixed the production seed path

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
       directory: __dirname + "/db/migrations"
     },
     seeds: {
-      directory: __dirname + "/db/seeds/production"
+      directory: __dirname + "/db/seeds"
     }
   }
 };
